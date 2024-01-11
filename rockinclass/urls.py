4c837d7171f85e800fcea84baf4f9fba14ca7223
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-01-11 21:21:48 +0000

    feat: add common app and index

--- rockinclass/urls.py
+++ rockinclass/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("common.urls")),
     path("admin/", admin.site.urls),
     path("booking/", include("booking.urls")),
 ]
