1f5462a83fe2892bb214d5d1d0a9995829c9dfd3
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-01-17 23:08:23 +0000

    fix: move custom css and fix static paths

--- rockinclass/settings.py
+++ rockinclass/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -147,4 +147,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
