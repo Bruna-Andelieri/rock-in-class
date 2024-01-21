b1c881bb25411ca77475ce19cb2465b16f1ac8c4
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-01-21 20:46:05 +0000

    feat: add tutor description on admin page

--- booking/admin.py
+++ booking/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from booking.models import Booking
 # Register your models here.
 
-admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ["student", "display_tutor", "booking_date", "booking_time", "message"]  # Lista todas as colunas do modelo
+
+    def display_tutor(self, obj):
+        return obj.tutor.name if obj.tutor else None  # Substitua pelo campo que deseja exibir
+    display_tutor.short_description = 'Tutor'  # Define um título para a coluna
+
+admin.site.register(Booking, BookingAdmin)
